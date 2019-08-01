441123778f4bdf7f52fab10cccd5286bef27cdee
Avery Peck <averypeck@northwesternmutual.com>
2019-08-01 09:50:21 -0500

    fix starting location

--- RESTful API Dev test/index.js
+++ RESTful API Dev test/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./routes')
-const port = 3000
+const port = 8080
 
 app.use(bodyParser.json())
 app.use(
--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -14,8 +14,8 @@ export class MapContainer extends Component {
         zoom={14}
         style={mapStyles}
         initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
+         lat: 43.040083,
+         lng: -87.900278
         }}
       />
     );
