263486145e4c9fb9430dd0081b4b63fc573355f6
Avery Peck <averypeck@northwesternmutual.com>
2019-08-01 13:21:14 -0500

    lat long

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './Home';
 import Header from './Header';
+import AboutMe from './AboutMe';
 
 function BasicExample() {
   return (
     <><Header/>
 
     <Router>
-
+        <Route exact path="/about-me" component={AboutMe} />
         <Route exact path="/" component={Home} />
     </Router></>
   );
