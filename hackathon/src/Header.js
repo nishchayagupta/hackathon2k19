6528d455940fb70a152d3abe594953ba44afa645
Avery Peck <averypeck@northwesternmutual.com>
2019-08-01 13:43:19 -0500

    drop down

--- hackathon/src/Header.js
+++ hackathon/src/Header.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { Navbar} from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import logo from './logo.png';
 
-const Header = (props) => {
- return (
-   <div>
-     <Navbar fluid style={{'background-color': 'lightblue', 'margin-bottom': '10px'}}>
-         <img style={{'height': '100px'}} src={logo}/>
-         <h1 className="display-6">Beyond Mason Street</h1>
-     </Navbar>
-   </div>
- );
-};
+export default class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dropdownOpen: false,
+        }
+        this.toggle = this.toggle.bind(this);
+    }
 
-export default Header;
+    toggle() {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+          }));
+    }
+
+    clickAbout() {
+        window.location.href = "/about-me";
+    }
+
+    clickHome() {
+        window.location.href = "/";
+    }
+
+ render() {
+    return (
+        <div>
+            <Navbar fluid style={{'background-color': 'lightblue', 'margin-bottom': '10px'}}>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                <DropdownToggle caret>
+                Menu
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem onClick={this.clickHome}>Home</DropdownItem>
+                    <DropdownItem onClick={this.clickAbout}>About Me</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+                <h1 className="display-6">Beyond Mason Street</h1>
+                <img style={{'height': '100px'}} src={logo}/>
+            </Navbar>
+        </div>
+        );
+    }
+};
